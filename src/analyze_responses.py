044563b182d5f347b8f2d2434220a6f1a495c45e
AdamVinestock <115546705+AdamVinestock@users.noreply.github.com>
2023-08-13 17:24:53 +0300

    .

--- src/analyze_responses.py
+++ src/analyze_responses.py
@@ -55,11 +55,12 @@ def plot_roc_auc(human_path, machine_path):
     h_df = pd.read_csv(human_path)
     m_df = pd.read_csv(machine_path)
     # Prepare labels: 1 for human, 0 for machine
-    labels = np.concatenate([np.ones_like(h_df[:,1]), np.zeros_like(m_df[:,1])])
-    print(f"shape of label: {labels.shape}")
+    labels = np.concatenate([np.ones_like(h_df.iloc[:, 1].values), np.zeros_like(m_df.iloc[:, 1].values)])
+    print(f"shape of labels: {labels.shape}")
 
     # Concatenate the responses
     responses = np.concatenate([h_df['response'], m_df['response']])
+    print(f"shape of responses: {responses.shape}")
 
     # Compute ROC curve
     fpr, tpr, _ = roc_curve(labels, responses)
